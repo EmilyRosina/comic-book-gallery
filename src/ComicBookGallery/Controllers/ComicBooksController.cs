d4720aa02daa1656a0e5e93ed7c7b0b76d2d38d2
Emily Carey <emilyrosinacarey@gmail.com>
2016-11-20 18:32:19 +0000

    Added Comic Book Detail View

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,11 +10,9 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
+
+
 }
